0ea0c9bc404ceeeb89455607b7fdebaa34028f93
MorozicH20 <125402976+MorozicH20@users.noreply.github.com>
2025-04-25 19:58:04 +0300

    Swich bindings

--- ModClass.cs
+++ ModClass.cs
@@ -5,20 +5,29 @@ using Modding;
 using UnityEngine;
 using UObject = UnityEngine.Object;
 using HelperForChallenge.PresetEquipment;
-using HelperForChallenge.Bindings;
-
+using ToggleableBindings;
 namespace HelperForChallenge
 {
     public class HelperForChallenge : Mod
     {
         internal static HelperForChallenge Instance;
 
-        private Binding _test;
-        public HelperForChallenge() : base(ModInfo.Name) { }
+        bool isCharms = false;
+        public string IDCharmsBinding = "ToggleableBindings::CharmsBinding";
+        
+        bool isNail = false;
+        public string IDNailBinding = "ToggleableBindings::NailBinding";
+        
+        bool isShell = false;
+        public string IDShellBinding = "ToggleableBindings::ShellBinding";
+        
+        bool isSoul = false;
+        public string IDSoulBinding = "ToggleableBindings::SoulBinding";
 
+
+        public HelperForChallenge() : base(ModInfo.Name) { }
         public override void Initialize()
         {
-            _test = new CharmBinding();
 
             base.Initialize();
             ModHooks.HeroUpdateHook += OnHeroUpdate;
@@ -27,13 +36,64 @@ namespace HelperForChallenge
 
         private void OnHeroUpdate()
         {
-            if (Input.GetKeyDown(KeyCode.Q))
+            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                if (_test.IsApplied)
-                    _test.Restored();
+                var Charms = BindingManager.GetBinding(IDCharmsBinding);
+                if (isCharms)
+                {
+                    isCharms = false;
+                    BindingManager.RestoreBinding(IDCharmsBinding);
+                }
                 else
-                    _test.Applied();
-
+                {
+                    isCharms = true;
+                    BindingManager.ApplyBinding(IDCharmsBinding);
+                }
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                var Charms = BindingManager.GetBinding(IDNailBinding);
+                if (isNail)
+                {
+                    isNail = false;
+                    BindingManager.RestoreBinding(IDNailBinding);
+                }
+                else
+                {
+                    isNail = true;
+                    BindingManager.ApplyBinding(IDNailBinding);
+                }
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                var Charms = BindingManager.GetBinding(IDShellBinding);
+                if (isShell)
+                {
+                    isShell = false;
+                    BindingManager.RestoreBinding(IDShellBinding);
+                }
+                else
+                {
+                    isShell = true;
+                    BindingManager.ApplyBinding(IDShellBinding);
+                }
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                var Charms = BindingManager.GetBinding(IDSoulBinding);
+                if (isSoul)
+                {
+                    isSoul = false;
+                    BindingManager.RestoreBinding(IDSoulBinding);
+                }
+                else
+                {
+                    isSoul = true;
+                    BindingManager.ApplyBinding(IDSoulBinding);
+                }
             }
 
         }
---
+++ Utility/CoroutineController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+namespace HelperForChallenge.Utility
+{
+    internal class CoroutineController : MonoBehaviour
+    {
+        private static CoroutineController? Instance { get; set; }
+
+        static CoroutineController()
+        {
+            Initialize();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+
+            var container = new GameObject("1");
+            Instance = container.AddComponent<CoroutineController>();
+        }
+        public static Coroutine Start(IEnumerator routine)
+        {
+            return Instance.StartCoroutine(routine);
+        }
+
+    }
+}
